117eee6ca96fb42469e82c7553a17dfb2ba685bd
Wouter <wouter@grijsloke.be>
2021-05-14 12:21:02 +0200

    Added item animations

--- src/main/java/trofers/data/ItemModels.java
+++ src/main/java/trofers/data/ItemModels.java
@@ -1,13 +1,13 @@
 package trofers.data;
 
-import trofers.Trofers;
-import trofers.common.init.ModBlocks;
 import net.minecraft.client.renderer.model.BlockModel;
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.client.model.generators.ModelBuilder;
 import net.minecraftforge.client.model.generators.ModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import trofers.Trofers;
+import trofers.common.init.ModBlocks;
 
 public class ItemModels extends ItemModelProvider {
 
@@ -17,22 +17,31 @@ public class ItemModels extends ItemModelProvider {
 
     @Override
     protected void registerModels() {
-        ModBlocks.TROPHIES.forEach(block -> {
-            ModelBuilder<?> builder = getBuilder(block.getId().getPath())
-                    .parent(new ModelFile.UncheckedModelFile("builtin/entity"))
-                    .guiLight(BlockModel.GuiLight.SIDE)
-                    .transforms()
+        ModBlocks.TROPHIES.forEach(block -> getBuilder(block.getId().getPath())
+                .parent(new ModelFile.UncheckedModelFile("builtin/entity"))
+                .guiLight(BlockModel.GuiLight.SIDE)
+                .transforms()
                     .transform(ModelBuilder.Perspective.GUI)
-                    .rotation(30, 225, 0).scale(0.625F).end()
+                        .rotation(30, 225, 0)
+                        .scale(0.625F)
+                    .end()
                     .transform(ModelBuilder.Perspective.FIXED)
-                    .scale(0.5F).end()
+                        .scale(0.5F)
+                    .end()
                     .transform(ModelBuilder.Perspective.GROUND)
-                    .translation(0, 3, 0).scale(0.25F).end()
+                        .translation(0, 3, 0)
+                        .scale(0.25F)
+                    .end()
                     .transform(ModelBuilder.Perspective.THIRDPERSON_RIGHT)
-                    .rotation(75, 45, 0).translation(0, 2.5F, 0).scale(0.375F).end()
+                        .rotation(75, 45, 0)
+                        .translation(0, 2, 1.5F)
+                        .scale(0.375F)
+                    .end()
                     .transform(ModelBuilder.Perspective.FIRSTPERSON_RIGHT)
-                    .rotation(0, 45, 0).scale(0.4F).end()
-                    .end();
-        });
+                        .rotation(0, 135, 0)
+                        .translation(0, 2, 0)
+                        .scale(0.4F)
+                    .end()
+                .end());
     }
 }
