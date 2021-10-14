f9e936079566e4d8a396e7464e923d9bf7db5ef9
Wouter Pauwels <37985539+ochotonida@users.noreply.github.com>
2021-10-14 23:53:41 +0200

    Backport Alex's Mobs trophies

--- src/main/java/trofers/data/LootModifiers.java
+++ src/main/java/trofers/data/LootModifiers.java
@@ -1,11 +1,19 @@
 package trofers.data;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import cpw.mods.modlauncher.api.LamdbaExceptionUtils;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.data.DirectoryCache;
+import net.minecraft.data.IDataProvider;
 import net.minecraft.entity.EntityType;
 import net.minecraft.loot.conditions.ILootCondition;
 import net.minecraft.loot.conditions.KilledByPlayer;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.common.data.GlobalLootModifierProvider;
+import net.minecraft.util.Tuple;
+import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
+import net.minecraftforge.common.loot.IGlobalLootModifier;
 import trofers.Trofers;
 import trofers.common.init.ModItems;
 import trofers.common.init.ModLootModifiers;
@@ -13,18 +21,25 @@ import trofers.common.loot.AddEntityTrophy;
 import trofers.common.loot.RandomTrophyChanceCondition;
 import trofers.common.trophy.Trophy;
 
+import java.nio.file.Path;
 import java.util.HashMap;
+import java.util.Map;
 
-public class LootModifiers extends GlobalLootModifierProvider {
+public class LootModifiers implements IDataProvider {
+
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    private final DataGenerator generator;
+    private final String modid;
+    private final Map<String, Tuple<GlobalLootModifierSerializer<?>, JsonObject>> toSerialize = new HashMap<>();
 
     private final Trophies trophies;
 
     public LootModifiers(DataGenerator generator, Trophies trophies) {
-        super(generator, Trofers.MODID);
         this.trophies = trophies;
+        this.generator = generator;
+        this.modid = Trofers.MODID;
     }
 
-    @Override
     protected void start() {
         for (String modid : trophies.trophies.keySet()) {
             HashMap<EntityType<?>, ResourceLocation> trophyMap = new HashMap<>();
@@ -46,5 +61,31 @@ public class LootModifiers extends GlobalLootModifierProvider {
             name = name + "_trophies";
             add(name, ModLootModifiers.ADD_ENTITY_TROPHY.get(), modifier);
         }
+    }
+
+    @Override
+    public void run(DirectoryCache cache) {
+        start();
+
+        String modPath = "data/" + modid + "/loot_modifiers/";
+
+        toSerialize.forEach(LamdbaExceptionUtils.rethrowBiConsumer((name, pair) -> {
+            Path modifierPath = generator.getOutputFolder().resolve(modPath + name + ".json");
+
+            JsonObject json = pair.getB();
+            // noinspection ConstantConditions
+            json.addProperty("type", pair.getA().getRegistryName().toString());
+
+            IDataProvider.save(GSON, cache, json, modifierPath);
+        }));
+    }
+
+    public <T extends IGlobalLootModifier> void add(String modifier, GlobalLootModifierSerializer<T> serializer, T instance) {
+        this.toSerialize.put(modifier, new Tuple<>(serializer, serializer.write(instance)));
+    }
+
+    @Override
+    public String getName() {
+        return "Global Loot Modifiers : " + modid;
     }
 }
