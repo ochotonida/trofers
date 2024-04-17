8b00fe3cfbc66d5347a147ec7cfa94557ed9b564
Wouter Pauwels <37985539+ochotonida@users.noreply.github.com>
2024-04-17 18:00:07 +0200

    Fix colors not showing with sodium and biome blending disabled, blend colors from nearest trophies

--- common/src/main/java/trofers/TrofersClient.java
+++ common/src/main/java/trofers/TrofersClient.java
@@ -1,61 +1,17 @@
 package trofers;
 
-import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.core.BlockPos;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.BlockAndTintGetter;
-import net.minecraft.world.level.block.state.BlockState;
-import org.jetbrains.annotations.Nullable;
-import trofers.block.entity.TrophyBlockEntity;
 import trofers.trophy.Trophy;
 
 public class TrofersClient {
 
-    private static int getTrophyColor(Trophy trophy, int index) {
+    public static int getTrophyColor(Trophy trophy, int index) {
         if (trophy != null) {
             if (index == 0) {
                 return trophy.colors().base();
             } else if (index == 1) {
                 return trophy.colors().accent();
             }
-        }
-        return 0xFFFFFF;
-    }
-
-    @SuppressWarnings("unused")
-    public static int getTrophyBlockColor(BlockState state, @Nullable BlockAndTintGetter level, @Nullable BlockPos pos, int index) {
-        // what the hell sodium 🤨🤔🤠
-        if (FabricLoader.getInstance().isModLoaded("sodium") && pos != null) {
-            pos = pos.offset(0, 1, 0);
-            if (level != null && level.getBlockEntity(pos) instanceof TrophyBlockEntity blockEntity) {
-                return getTrophyColor(blockEntity.getTrophy(), index);
-            }
-            for (int i = -1; i <= 1; i++) {
-                for (int j = -1; j <= 1; j++) {
-                    if (i != 0 && j != 0) {
-                        continue;
-                    }
-                    BlockPos p = pos.offset(i, 0, j);
-                    if (level != null && level.getBlockEntity(p) instanceof TrophyBlockEntity blockEntity) {
-                        return getTrophyColor(blockEntity.getTrophy(), index);
-                    }
-                }
-            }
-            for (int i = -1; i <= 1; i++) {
-                for (int j = -1; j <= 1; j++) {
-                    if (i == 0 || j == 0) {
-                        continue;
-                    }
-                    BlockPos p = pos.offset(i, 0, j);
-                    if (level != null && level.getBlockEntity(p) instanceof TrophyBlockEntity blockEntity) {
-                        return getTrophyColor(blockEntity.getTrophy(), index);
-                    }
-                }
-            }
-        }
-
-        if (level != null && pos != null && level.getBlockEntity(pos) instanceof TrophyBlockEntity blockEntity) {
-            return getTrophyColor(blockEntity.getTrophy(), index);
         }
         return 0xFFFFFF;
     }
--- fabric/src/main/java/trofers/fabric/TrofersFabricClient.java
+++ fabric/src/main/java/trofers/fabric/TrofersFabricClient.java
@@ -9,10 +9,15 @@ import net.fabricmc.fabric.api.resource.IdentifiableResourceReloadListener;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
+import net.minecraft.core.BlockPos;
 import net.minecraft.server.packs.PackType;
+import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
 import trofers.TrofersClient;
 import trofers.block.TrophyBlock;
+import trofers.block.entity.TrophyBlockEntity;
 import trofers.block.entity.TrophyBlockEntityRenderer;
 import trofers.item.TrophyItemRenderer;
 import trofers.registry.ModBlockEntityTypes;
@@ -42,9 +47,51 @@ public class TrofersFabricClient implements ClientModInitializer {
 
     private static void registerColorHandlers() {
         for (Supplier<TrophyBlock> trophy : ModBlocks.TROPHIES) {
-            ColorProviderRegistry.BLOCK.register(TrofersClient::getTrophyBlockColor, trophy.get());
+            ColorProviderRegistry.BLOCK.register(TrofersFabricClient::getTrophyBlockColor, trophy.get());
             ColorProviderRegistry.ITEM.register(TrofersClient::getTrophyItemColor, trophy.get());
         }
+    }
+
+    @SuppressWarnings("unused")
+    public static int getTrophyBlockColor(BlockState state, @Nullable BlockAndTintGetter level, @Nullable BlockPos pos, int index) {
+        if (level == null || pos == null) {
+            return 0xFFFFFF;
+        } else if (level.getBlockEntity(pos) instanceof TrophyBlockEntity blockEntity) {
+            return TrofersClient.getTrophyColor(blockEntity.getTrophy(), index);
+        }
+
+        // what the hell sodium 🤨🤔🤠
+        pos = pos.offset(0, 1, 0);
+        if (level.getBlockEntity(pos) instanceof TrophyBlockEntity blockEntity) {
+            return TrofersClient.getTrophyColor(blockEntity.getTrophy(), index);
+        }
+        int count = 0;
+        int[] colors = new int[9];
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                BlockPos p = pos.offset(i, 0, j);
+                if (level.getBlockEntity(p) instanceof TrophyBlockEntity blockEntity) {
+                    colors[count] = TrofersClient.getTrophyColor(blockEntity.getTrophy(), index);
+                    count += 1;
+                }
+            }
+        }
+        return blendColors(colors, count);
+    }
+
+    private static int blendColors(int[] colors, int count) {
+        if (count == 0) {
+            return 0xFFFFFF;
+        }
+        int result = 0;
+        for (int i = 0; i <= 16; i += 8) {
+            int c = 0;
+            for (int j = 0; j < count; j++) {
+                c += Math.pow((colors[j] >> i) & 0xFF, 2);
+            }
+            result += Math.round(Math.sqrt(c / (double) count)) << i;
+        }
+        return result;
     }
 
     @SuppressWarnings("ConstantConditions")
